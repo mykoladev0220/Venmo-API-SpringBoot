fd866539de915a41d2bbfad2c5c0bd46e5895052
mykoladev0220 <mykolademchukdev@gmail.com>
2021-03-20 13:43:48 -0400

    Update AccountService.java

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
@@ -13,7 +13,7 @@ import com.techelevator.tenmo.models.Account;
 import com.techelevator.tenmo.models.AuthenticatedUser;
 
 public class AccountService {
-	
+
 	private final String BASE_URL;
 	private final RestTemplate restTemplate = new RestTemplate();
 	public AuthenticatedUser currentUser;
@@ -27,17 +27,13 @@ public class AccountService {
 
 		Account account = new Account();
 		int accountId = 0;
-		accountId = restTemplate.exchange(BASE_URL + "accounts/" + userId, HttpMethod.GET,
-				makeAuthEntity(), int.class).getBody();
-
+		accountId = restTemplate.exchange(BASE_URL + "accounts/" + userId, HttpMethod.GET, makeAuthEntity(), int.class).getBody();
 		account.setAccount_id(accountId);
 
 		BigDecimal balanceBD = new BigDecimal("0");
-		balanceBD = restTemplate.exchange(BASE_URL + "accounts/balance/" + userId, HttpMethod.GET,
-				makeAuthEntity(), BigDecimal.class).getBody();
+		balanceBD = restTemplate.exchange(BASE_URL + "accounts/balance/" + userId, HttpMethod.GET, makeAuthEntity(), BigDecimal.class).getBody();
 
 		account.setBalance(balanceBD.add(amountToChange));
-
 		account.setUser_id(userId);
 
 		restTemplate.exchange(BASE_URL + "/accounts/" + userId, HttpMethod.PUT, makeAccountEntity(account), Account.class);
@@ -47,29 +43,26 @@ public class AccountService {
 
 		Account account = new Account();
 		int accountId = 0;
-		accountId = restTemplate.exchange(BASE_URL + "accounts/" + userId, HttpMethod.GET,
-				makeAuthEntity(), int.class).getBody();
-
+		accountId = restTemplate.exchange(BASE_URL + "accounts/" + userId, HttpMethod.GET, makeAuthEntity(), int.class).getBody();
 		account.setAccount_id(accountId);
 
 		BigDecimal balanceBD = new BigDecimal("0");
 		balanceBD = restTemplate.exchange(BASE_URL + "accounts/balance/" + userId, HttpMethod.GET,
 				makeAuthEntity(), BigDecimal.class).getBody();
-		// have to retrieve current balance
-		account.setBalance(balanceBD.subtract(amountToChange));
 
+		account.setBalance(balanceBD.subtract(amountToChange));
 		account.setUser_id(userId);
 
 		restTemplate.exchange(BASE_URL + "/accounts/" + userId, HttpMethod.PUT, makeAccountEntity(account), Account.class);
 	}
-	
+
 	public HttpEntity makeAuthEntity() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setBearerAuth(App.AUTH_TOKEN);
 		HttpEntity entity = new HttpEntity<>(headers);
 		return entity;
 	}
-	
+
 	public HttpEntity<Account> makeAccountEntity(Account account) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
@@ -77,5 +70,5 @@ public class AccountService {
 		HttpEntity<Account> entity = new HttpEntity<>(account, headers);
 		return entity;
 	}
-	
+
 }
