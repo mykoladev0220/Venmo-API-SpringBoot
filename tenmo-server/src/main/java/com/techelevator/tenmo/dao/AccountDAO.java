8ddca84d9a61466abacd137eccc74c61ba12ff35
mykoladev0220 <mykolademchukdev@gmail.com>
2021-03-21 18:59:23 -0400

    Update AccountDAO.java

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDAO.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDAO.java
@@ -7,11 +7,11 @@ import com.techelevator.tenmo.model.Account;
 
 public interface AccountDAO {
 
-	public Account getAccountById(int account_id);
+	public Account getAccountByAccountId(int account_id);
 	
 	public int getAccountIdByUserId(int user_id);
 	
-	public void updateAccountById(Account account, int id);
+	public void updateAccountByUserId(Account account, int user_id);
 	
 	public BigDecimal getAccountBalanceByUserId(int user_id);
 }
