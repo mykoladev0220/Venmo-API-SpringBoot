f91a3b42daa87baf4667d8e32db7d29ff9dc07f4
mykoladev0220 <mykolademchukdev@gmail.com>
2021-03-22 08:56:04 -0400

    Update TransferService.java

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
-import com.techelevator.tenmo.App;
-import com.techelevator.tenmo.models.Account;
 import com.techelevator.tenmo.models.AuthenticatedUser;
 import com.techelevator.tenmo.models.Transfer;
 
